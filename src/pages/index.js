86f345cc5dd290c58987dbbfa68d7dab899d260e
unknown <ianchen6501@gmail.com>
2021-07-04 16:25:34 +0800

    feat/homepage add urls

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Layout from "../components/layout"
 import * as indexStyles from "../styles/index.module.css"
 import "../styles/index-animation.scss"
 
+
+
 export default function Home() {
 
   return (
@@ -12,6 +14,11 @@ export default function Home() {
           <div className={indexStyles.introText}>
             <h3 className={indexStyles.title}>Ian_義明 獨立設計人</h3>
             <p className={indexStyles.paragraph}>Web design 網頁設計規劃<br/>Architecture 建築設計規劃<br/>Interior Design 室內設計規劃<br/></p>
+            <h3 className={indexStyles.title}>相關連結</h3>
+            <div className={indexStyles.urls}>
+              <a target="popup" href="https://github.com/ianchen6501"><span>github</span></a>
+              <a target="popup" href="https://codepen.io/ianchen6501"><span>codepen</span></a>
+            </div>
           </div>
           <div className={indexStyles.devideLine} />
           {/* <BackgroundImg src={imgsArray[imgIndex]}></BackgroundImg> */}
